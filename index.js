b5e78e71bc7caeff4c4ec299b14530ffec801ad9
Tal Hayut <talhayut.dev@gmail.com>
2020-03-03 08:13:15 +0200

    refactor: major refactor npm card

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const chars = {
 let coloredChars = {};
 let currentRandom = undefined;
 
+const bolder = word => chalk.default.bold(word);
+const link = link => chalk.gray(link);
+const repeat = (rep, times) => rep.repeat(times);
 const randomGradient = string => {
   const randoms = info.colors.map(color => {
     return chalk.hex(color)(string);
@@ -38,17 +41,38 @@ const randomGradient = string => {
   if (!currentRandom && currentRandom !== 0) {
     currentRandom = Math.floor(Math.random() * randoms.length);
   }
-  
+
   return randoms[currentRandom];
 };
+const web = (web) => {
+  const url = web.split("/");
+  const path = url[url.length - 1];
+  const domain = url.slice(1, -1);
+
+  return (
+    repeat(" ", 12) +
+    chalk.whiteBright("web") +
+    repeat(" ", 7) +
+    link(`https://${domain[1]}/`) +
+    chalk.hex("#6ce2e2")(path) +
+    repeat(" ", 13)
+  );
+};
+
+const createEntry = (name, space) => {
+  const item = info.social[name];
+  const coloredUser = item.color ? chalk.hex(item.color)(item.user) : item.user
+  return repeat(" ", 12) +
+    chalk.whiteBright(name) +
+    repeat(" ", space) +
+    link(item.url) +
+    coloredUser +
+    repeat(" ", 11);
+}
 
 Object.keys(chars).forEach(key => {
   coloredChars[key] = randomGradient(chars[key]);
 });
-
-const bolder = word => chalk.default.bold(word);
-const link = link => chalk.gray(link);
-const repeat = (rep, times) => rep.repeat(times);
 
 currentRandom = undefined;
 const table = new Table({ chars: coloredChars });
@@ -58,77 +82,21 @@ const work = bolder(
   chalk.whiteBright(`${info.occupation} ${coloredChars.top.replace("═", "@")} ${chalk.red(info.work)}`)
 );
 
-const github =
-  repeat(" ", 12) +
-  chalk.whiteBright("Github") +
-  repeat(" ", 4) +
-  link("https://github.com/") +
-  chalk.hex("#bcbcbc")(info.github) +
-  repeat(" ", 11);
-
-const gitlab =
-  repeat(" ", 12) +
-  chalk.whiteBright("Gitlab") +
-  repeat(" ", 4) +
-  link("https://gitlab.com/") +
-  chalk.hex("#FCA326")(info.gitlab) +
-  repeat(" ", 11);
-const linkedIn =
-  repeat(" ", 10) +
-  chalk.whiteBright("LinkedIn") +
-  repeat(" ", 4) +
-  link("https://linkedin.com/in/") +
-  chalk.hex("#0C65B3")(info.linkedIn) +
-  repeat(" ", 9);
-
-const npm =
-  repeat(" ", 15) +
-  chalk.whiteBright("npm") +
-  repeat(" ", 4) +
-  link("https://npmjs.com/~") +
-  chalk.red(info.npm) +
-
-  repeat(" ", 11);
-
-  const dev =
-  repeat(" ", 15) +
-  chalk.whiteBright("DEV") +
-  repeat(" ", 4) +
-  link("https://dev.to/") +
-  chalk.whiteBright(info.github) +
-  repeat(" ", 11);
+table.push([{ content: `\n${chalk.bold(name.trim())}\n`, hAlign: "center" }]);
+table.push([{ content: `${work}\n`, hAlign: "center" }]);
 
 
-const web = ({ web }) => {
-  const url = web.split("/");
-  const path = url[url.length - 1];
-  const domain = url.slice(1, -1);
+Object.keys(info.social).map(name => {
+  if (name === "web") {
+    table.push([{ content: web(info.social[name].url) }]);
+    return;
+  }
+  const space = name.length > 6 ? 2 : (name.length <= 3 ? 7 : 4);
+  const entry = createEntry(name, space);
+  table.push([{ content: entry, vAlign: 'center' }]);
+});
 
-  return (
-    repeat(" ", 15) +
-    chalk.whiteBright("Web") +
-    repeat(" ", 4) +
-    link(`https://${domain[1]}/`) +
-    chalk.hex("#6ce2e2")(path) +
-    repeat(" ", 11)
-  );
-};
-
-const card = `${chalk.red("npx")} ${package.name}`;
-
-table.push([" "]);
-table.push([{ content: gradient.vice(name.trim()), hAlign: "center" }]);
-table.push([" "]);
-table.push([{ content: work, hAlign: "center" }]);
-table.push([" "]);
-table.push([{ content: github }]);
-table.push([{ content: gitlab }]);
-table.push([{ content: npm }]);
-table.push([{ content: dev }]);
-table.push([{ content: linkedIn }]);
-table.push([{ content: web(info) }]);
-table.push([" "]);
-table.push([{ content: card, hAlign: "center" }]);
-table.push([" "]);
+const npx = `${chalk.red("npx")} ${package.name}`;
+table.push([{ content: `\n${npx}\n`, hAlign: 'center', vAlign: 'center' }]);
 
 console.log(table.toString());
